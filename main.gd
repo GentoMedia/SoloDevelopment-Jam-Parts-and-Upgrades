e1afa3a919512034327ace3b2d6edc4a91b20080
Darien Sebastion Charles <53331146+GentoMedia@users.noreply.github.com>
2025-05-06 17:18:39 +0900

    Prototype Completed and Exported

--- main.gd
+++ main.gd
@@ -12,6 +12,10 @@ func _ready() -> void:
 	toggle_pause()
 
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle_pause()
+
 
 func update_materials(resource : String, amount : int):
 	materials[resource] += amount
@@ -29,19 +33,22 @@ func _on_timer_timeout() -> void:
 		if time_to_upgrade <= 0:
 			time_to_upgrade = 0
 			$Garden/UpgradeSpot/UpgradeIndicator.visible = true
-			if $Garden/ControlIndicator:
-				$Garden/ControlIndicator.queue_free()
 			$UpgradeReadyBell.play()
-		$HUD/ProgressBar.value = (1 - time_to_upgrade/(float(upgrade_base_timer*(upgrades+1))))*100
+		$HUD/ProgressBar.value = (1 - time_to_upgrade/(float(upgrade_base_timer + (upgrades * 5))))*100
 	
 	if randi_range(upgrades, 12) >= 10:
 		$Garden.make_weed()
 
 func upgrade_update(upgrade_amount : int):
 	upgrades = upgrade_amount
-	time_to_upgrade = upgrade_base_timer * (upgrades + 1)
+	time_to_upgrade = upgrade_base_timer + (upgrades * 5)
 	$HUD/ProgressBar.value = 0
 
 func toggle_pause():
 	get_tree().paused = !get_tree().paused
 	$HUD/PauseScrene.visible = get_tree().paused
+	if get_tree().paused:
+		$HUD/PauseScrene/Button.grab_focus()
+
+func exit_game():
+	get_tree().quit()
